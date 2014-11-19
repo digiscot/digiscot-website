8c50270799c45a54f7130d93e7b90d2a55a5c429
clry2 <chris@yiu.co.uk>
2014-11-19 22:30:49 +0000

    Fix scroll flicker

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 $(function() {
 
-  $('a').not('.dropdown-toggle').click(function(){
+  $('a#splash-down').click(function(e){
+      e.preventDefault();
       $('html, body').animate({
       scrollTop: $( $.attr(this, 'href') ).offset().top
       }, 500);  
-    });
-
-  $('.nav a').not('.dropdown-toggle').click(function(){ 
-      if($('.navbar-toggle').css('display') !='none'){
-        $(".navbar-toggle").trigger( "click" );
-      }
     });
     
 });
