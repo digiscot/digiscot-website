29b6500542f7262beecf82242a045d195ff6e802
cccec <cccec@users.noreply.github.com>
2015-07-16 13:26:34 +0100

    Challenge Fund Forms in HTML format, with CSS and JS

---
+++ ChallengeFundForms/Scripts/ChallengeFund.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+
+
+});
+
+function get(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+}
+
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+function makeId() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
---
+++ ChallengeFundForms/Scripts/ChallengeFundForm.js
@@ -0,0 +1,218 @@
+﻿$(document).ready(function () {
+
+    authenticate();
+    
+    var options = {
+        beforeSubmit: function () {
+            $(".required-field").removeClass("required-field");
+            $('#ProcessingPanel').show();
+        },
+        success: function (response) {
+            if (response === "") {
+                OnSuccessCall();
+            } else {
+                OnErrorCall('Problems reported while saving your data:  ' + response);
+            }
+            $('#ProcessingPanel').hide();
+        },
+        error: function (response) {
+            OnErrorCall('Technical error');
+            $('#ProcessingPanel').hide();
+        },
+        url: this.action,
+        type: this.method
+    };
+
+    // bind to the form's submit event
+    $('#ChallengeFundForm').submit(function () {
+        // inside event callbacks 'this' is the DOM element so we first
+        // wrap it in a jQuery object and then invoke ajaxSubmit
+        $(this).ajaxSubmit(options);
+        return false; //prevent standard browser submit and page navigation
+    });
+
+});
+
+function authenticate() {
+    var key = getCookie("ChallengeFundApplicationKey");
+    $.ajax({
+        type: 'GET',
+        dataType: "json",
+        url: '/ChallengeFund/Get?key=' + key,
+        success: function (data) {
+            createCookie("ChallengeFundApplicationKey", data.AuthenticationKey, 60);
+            //console.log("LOAD", data);
+            for (key in data) {
+
+                $("#" + key).val(data[key]); //inputboxes, textareas & comboboxes
+
+                $('#cb' + key).prop('checked', data[key]); //checkboxes
+                $('#cb' + key).trigger("change");
+
+                if (key === "Items") {
+                    var items = data[key];
+                    for (var i = 0; i < items.length; i++) {
+                        var item = items[i];
+                        addRowToTable('#' + item.Type + 'Table', '#Default' + item.Type + 'Row', item);
+                    }
+                }
+            }
+            loadData(data);
+        },
+        error: function (data) {
+            console.log("ERROR", data);
+            //window.location.href = "Register.html";
+        }
+    });
+}
+
+function deauthenticate() {
+    eraseCookie("ChallengeFundApplicationKey");
+}
+
+function appendCheckBoxesToObject(objPrefix, arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var row = "<div class='checkbox col-md-3 col-xs-6' style='margin-top:4px;'><label class='col'>";
+        row += "<input name='" + objPrefix + "[]' id='" + objPrefix + i + "' type='checkbox' value='" + arr[i] + "'";
+
+        if (arr[i].toLowerCase() === "other, please specify") {
+            row += " onchange='otherOptionChanged(this, $(\"#" + objPrefix + "CommentRow\"));'";
+        }
+        row += ">" + arr[i];
+        row += "</label></div>";
+        $("#" + objPrefix + "Row").append(row + "\n");
+    }
+}
+
+function appendOptionValuesToDropDown(dropDownId, arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var row = "<option value=\"" + arr[i] + "\">" + arr[i] + "</option>";
+        $("#" + dropDownId).append(row + "\n");
+    }
+}
+
+function addRowToTable(tableId, defaultRowId, itemData, maxRows) {
+
+    if (maxRows > 0) {
+        maxRows += 2; //first row & totals are already there as well
+        var rowCount = $(tableId + ' tr').length;
+        if (rowCount >= maxRows) return;
+    }
+    
+    var templateRow = $(defaultRowId).html();
+    if (templateRow) {
+        var id = makeId();
+        var htmlRow = templateRow.replace(/XXX/g, id);
+
+        if (!itemData) { //default values:
+            itemData = {
+                Month: '',
+                Description: '',
+                Amount: '',
+                Number: ''
+            };
+        }
+
+        htmlRow = htmlRow.replace("@Description", (itemData.Description) ? (itemData.Description) : '' );
+        htmlRow = htmlRow.replace("@Amount", (itemData.Amount) ? (itemData.Amount) : '' );
+        htmlRow = htmlRow.replace("@Number", (itemData.Number) ? (itemData.Number) : '');
+        htmlRow = htmlRow.replace('"' + itemData.Month + '"', '"' + itemData.Month + '" selected');
+
+        var deleteRowCell = "<td><input type='button' class='delete-row' onClick='removeRowFromTable(\"" +
+                                tableId + "\",\"" + id + "\");' " + "value='X'></td>";
+
+        $(tableId + " tr:last").before("<tr id='" + id + "'>" + htmlRow + deleteRowCell + "</tr>");
+    }
+
+    markTableRows(tableId);
+    if (typeof (callback) == "calculateTotals") calculateTotals();
+}
+
+function removeRowFromTable(tableId, id) {
+    $('table' + tableId + ' tr#' + id).remove();
+    //console.log(id);
+    markTableRows(tableId);
+    calculateTotals();
+}
+
+function markTableRows(tableId) {
+    //console.log(tableId);
+    $(tableId + ' > tbody  > tr').each(function (i, tr) {
+        var countCell = $("td", tr).first();
+        $(countCell).html(i + 1);
+        return i;
+    });
+}
+
+function getTotalsForClass(className) {
+    var sum = 0;
+    var i = 0;
+    $('.' + className).each(function () {
+        if (i > 0) { //leave out the template (first one, so i == 0)
+            var value = parseFloat($(this).val());
+            if (value > 0 || value < 0) {
+                sum += value;
+            }
+        }
+        i++;
+    });
+    return sum;
+}
+
+function OnErrorCall(error) {
+    window.scrollTo(0, 0); //scroll to the top of the screen to show the error message
+
+    //TODO - is logo mandatory?
+    //if (!$("#logoFile").val()) {
+    //    $("#logoFileValidationError").show();
+    //}
+
+    $("#ResultSection").html('<div class="alert alert-danger"><button type="button" class="close">&times;</button><br />' +
+                               error + '</div>');
+    window.setTimeout(function () {
+        $(".alert").fadeTo(500, 0).slideUp(500, function () {
+            //$(this).remove();
+        });
+    }, 5000);
+    $('.alert .close').on("click", function (e) {
+        $(this).parent().fadeTo(500, 0).slideUp(500);
+    });
+}
+
+function cbChanged(checkbox, prefixOfControlToToggle) {
+    //console.log($("#" + prefixOfControlToToggle + "Label"));
+    $(checkbox).next().val(checkbox.checked); //mark the hidden input value to be sent with the form
+    if (prefixOfControlToToggle) { //if we want to make some more controls visible/hidden
+        if (checkbox.checked) {
+            $("#" + prefixOfControlToToggle + "Label").show();
+            $("#" + prefixOfControlToToggle + "Layer").show();
+        } else {
+            $("#" + prefixOfControlToToggle + "Label").hide();
+            $("#" + prefixOfControlToToggle + "Layer").hide();
+        }
+    }
+}
+
+function ensureTableHasOneEmptyRow(tablePrefix, maxRows) {
+    var rowCount = document.getElementById(tablePrefix + "Table").getElementsByTagName("tr").length;
+    if (rowCount <= 2) {
+        addRowToTable('#' + tablePrefix + 'Table', '#Default' + tablePrefix + 'Row', maxRows);
+    }
+}
+
+function isInteger(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+function isDecimal(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode != 46)
+        return false;
+
+    return true;
+}
