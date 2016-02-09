0fc0ab93d0228a8012d56fd0c5ce0a7e89b963fe
aly271287 <alastairgwatson@gmail.com>
2016-02-09 16:49:22 +0000

    keys

--- twitter/api/config.php
+++ twitter/api/config.php
@@ -4,12 +4,12 @@
      */
     
     // Consumer Key
-    define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
-    define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+    define('CONSUMER_KEY', 'XJNKmlrB51ttczl1J84EGLT2B');
+    define('CONSUMER_SECRET', 'ZrgCjH2W2NbvE05gfLF72JmYL2X9iCMJOmGLeQwQKOYQWpN3Nx');
 
     // User Access Token
-    define('ACCESS_TOKEN', 'ACCESS_TOKEN_HERE');
-    define('ACCESS_SECRET', 'ACCESS_SECRET_HERE');
+    define('ACCESS_TOKEN', '106991633-wJvNFWulqgKJjcLG1L9A8Bx1BMbmLk96IP4W4pyS');
+    define('ACCESS_SECRET', 'lEMu1O79EHZA2nS1HLJwnlL7vGjrXBP496k1hPCDzx986');
 	
 	// Cache Settings
 	define('CACHE_ENABLED', false);
