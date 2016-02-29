9155eaf054d9a1174a67a687f91739ddadfad6f0
Calum MacÙisdean <calummacuisdean@users.noreply.github.com>
2016-02-29 11:49:00 +0000

    Update app.js
    changed jquery method for stopping negative number data entry

--- questionnaire/scripts/app.js
+++ questionnaire/scripts/app.js
@@ -1,10 +1,8 @@
 $( document ).ready(function() {
-  $('input[type="number"]').onkeydown = function(e) {
-      if(!((e.keyCode > 95 && e.keyCode < 106)
-        || (e.keyCode > 47 && e.keyCode < 58) 
-        || e.keyCode == 8)) {
-          return false;
-      }
-  }
+  $('input[type="number"]').keypress(function(event) {
+    if ( event.which == 45 || event.which == 189 ) {
+        event.preventDefault();
+     }
+  });
   $("#QuestionnaireForm").validate();
 });
