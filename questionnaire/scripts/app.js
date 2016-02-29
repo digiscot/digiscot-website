f4c3537f53695d491acec5919ebaf7a6f356c866
Calum MacÙisdean <calummacuisdean@users.noreply.github.com>
2016-02-29 11:40:42 +0000

    Update app.js
    added function to stop negative numbers being entered in form fields

--- questionnaire/scripts/app.js
+++ questionnaire/scripts/app.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
+  $('number').onkeydown = function(e) {
+      if(!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58) 
+        || e.keyCode == 8)) {
+          return false;
+      }
+  }
   $("#QuestionnaireForm").validate();
 });
