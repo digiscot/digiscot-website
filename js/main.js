109e8f5ca60a8025f323e49edb1703dc09ca5a03
Calum MacUisdean <calum@martinmacuisdean.eu>
2016-03-15 13:49:20 +0000

    redirect to new charter site

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 $(function() {
 
   $('a#splash-down').click(function(e){
-      e.preventDefault();
-      $('html, body').animate({
+    e.preventDefault();
+    $('html, body').animate({
       scrollTop: $( $.attr(this, 'href') ).offset().top
-      }, 500);  
-    });
+    }, 500);  
+  });
   
-  console.log(window.location);
-
   if (window.location.href == 'http://digital.scvo.org.uk/charter/') {
     window.location.href = 'http://charter.scvo.org.uk/';
   }
