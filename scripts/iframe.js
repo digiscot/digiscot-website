9c8ba1734e300b7e4aad025b276ec849bd0db365
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-07-24 12:22:56 +0100

    Added iframe resize listener

---
+++ scripts/iframe.js
@@ -0,0 +1,9 @@
+window.addEventListener("message", (event) => {
+    if (event.data.hasOwnProperty('event')) {
+        switch (event.data.event) {
+            case ('resize'):
+            $('iframe[src*="' + event.origin + '"]').css('height', event.data.height);
+            break;
+        }
+    }
+}, false);
