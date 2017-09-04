f94c4d6c15eeda29fa40ead3b7eae6acb3508f1b
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-09-04 10:00:38 +0100

    Added link to questionnaire

--- scripts/iframe.js
+++ scripts/iframe.js
@@ -2,7 +2,7 @@ window.addEventListener("message", (event) => {
     if (event.data.hasOwnProperty('event')) {
         switch (event.data.event) {
             case ('resize'):
-            $('iframe[src*="' + event.origin + '"]').css('height', event.data.height);
+            $('iframe[src*="' + event.origin + '"]').css('height', event.data.height+30);
             break;
         }
     }
